29d61e9b2ff0431da9c3978f7fceb4dd8071288c
franklim.paulino <franklim.paulino@detran.df.gov.br>
2021-09-05 23:00:51 -0300

    Create de Curso Front_end

--- html/cadastrar.php
+++ html/cadastrar.php
@@ -1,7 +1,91 @@
-<form method="get" action="">
-    <input type="text" name="nome" placeholder="digite seu Nome aqui">
-    <input type="e-mail" name="email" placeholder="Digite seu e-mail">
-    <input type="submit" name="enviar" value="Enviar">
-</form>
-</br>
-<a href="/">Voltar</a>
+<?php
+spl_autoload_register(function ($className) {
+    $extension =  spl_autoload_extensions();
+    require_once (__DIR__ . '/Vendor/' . $className . '.php');
+}
+);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html" charset=utf-8" />
+    <title>Desafio-leo</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+</head>
+<body>
+<div  class="rounded-t-lg overflow-hidden border-t border-l border-r border-gray-400 flex justify-center p-8">
+    <form class="w-full max-w-lg">
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w px-3 mb-6 md:mb-0">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="curso[nome]">
+                    Nome
+                </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                       id="curso[nome]" type="text" placeholder="Php">
+                <p class="text-red-500 text-xs italic">Preencha com nome do curso</p>
+            </div>
+
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="curso[descricao]">
+                    Descrição
+                </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                       id="curso[descricao]" type="text" >
+            </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="curso[vagas]">
+                    Quantidade de vagas
+                </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                       min="0" id="curso[vagas]" type="number">
+            </div>
+            <div class="w-full md:w-1/2 px-3">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="curso[vagas_preenchidas]">
+                    Quantidade de vagas preenchidas
+                </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                       id="curso[vagas_preenchidas]" type="number" min="0">
+             </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="curso[inicio]">
+                    Data de início
+                </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                       id="curso[inicio]" type="date">
+            </div>
+            <div class="w-full md:w-1/2 px-3">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="curso[fim]">
+                    Fim
+                </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                       id="curso[fim]" type="date">
+            </div>
+        </div>
+        <div class="md:flex md:items-center">
+                <div class="md:w-1/3">
+                    <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                        Cadastrar Curso
+                    </button>
+                </div>
+                <div class="md:w-2/3">
+                    <a href="/" class="shadow bg-blue-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                        Voltar
+                    </a>
+                </div>
+            </div>
+
+
+    </form>
+
+</div>
+
+
+</body>
+</html>
--- html/index.php
+++ html/index.php
@@ -1,5 +1,4 @@
 <?php
-header('Content-type: text/html; charset=utf-8');
 spl_autoload_register(function ($className) {
     $extension =  spl_autoload_extensions();
     require_once (__DIR__ . '/Vendor/' . $className . '.php');
@@ -9,9 +8,10 @@ spl_autoload_register(function ($className) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
     <meta http-equiv="Content-Type" content="text/html" charset=utf-8" />
     <title>Desafio-leo</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
 </head>
 <body>
 <?php
@@ -21,7 +21,8 @@ foreach ($curso->getByAll() as $obj):
 ?>
     <p><?=  $obj->nome ?></p>
 <?endforeach ?>
-<a href="/cadastrar.php">Cadastrar Curso</a>
+<a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+   href="/cadastrar.php">Cadastrar Curso</a>
 
 </body>
 </html>
